a375e2a7986ffa06193f8f7ea218831ef9f9611d
plusmate <psejin1478@gmail.com>
2025-03-26 17:20:43 +0900

    Redis 기본 설정 구현

--- src/main/java/com/plusproject/PlusProjectApplication.java
+++ src/main/java/com/plusproject/PlusProjectApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlusProjectApplication {
+public class
+PlusProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlusProjectApplication.class, args);
