a58231ce3e07f7c7fab36cda966af1316cc8c6ea
JiHo Jeon <wjswlgh96@naver.com>
2025-03-28 14:17:16 +0900

    test(#11): 테스트 환경 구성

--- src/test/java/com/plusproject/ControllerTestSupport.java
+++ src/test/java/com/plusproject/ControllerTestSupport.java
@@ -9,10 +9,12 @@ import com.plusproject.domain.usercoupon.service.UserCouponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class ControllerTestSupport {
 
--- src/test/java/com/plusproject/SpringBootTestSupport.java
+++ src/test/java/com/plusproject/SpringBootTestSupport.java
@@ -1,7 +1,9 @@
 package com.plusproject;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class SpringBootTestSupport {
 }
